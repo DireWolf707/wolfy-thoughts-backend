bbb6239a57ca9ce33fee6f2404ffc9ee9fa75374
DireWolf707 <rahulsood707@gmail.com>
2023-05-01 13:02:53 +0530

    added validators

---
+++ src/validators/index.js
@@ -0,0 +1,18 @@
+import { z } from "zod"
+import { AppError } from "../utils"
+
+const imageMimeTypes = ["image/jpeg", "image/png"]
+export const ImageInput = (image) => {
+  if (!image) throw new AppError("no image file uploaded", 400)
+  if (!imageMimeTypes.includes(image.mimetype)) throw new AppError("Only .jpg, .jpeg and .png formats are supported", 400)
+  return image
+}
+
+export const UserInput = z.object({
+  name: z.string().trim().min(2).max(200),
+  username: z.string().trim().min(2).max(195),
+})
+
+export const UserSignupInput = UserInput.extend({
+  email: z.string().trim().email(),
+})
