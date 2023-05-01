3e5d7a76c63dc2ecc413f78f6e24d7292721524d
DireWolf707 <rahulsood707@gmail.com>
2023-05-01 13:02:32 +0530

    added routers

---
+++ src/routers/userRouter.js
@@ -0,0 +1,20 @@
+import express from "express"
+import passport from "passport"
+import { getProfile, updateProfile, updateAvatar, deleteAvatar, logout } from "../controllers/userController"
+import { isAuthenticated } from "../middlewares/auth"
+
+const router = express.Router()
+router.get("/login/google", passport.authenticate("google"))
+router.get(
+  "/oauth2/redirect/google",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: process.env.CLIENT_URL + "/500",
+  })
+)
+router.post("/logout", logout)
+router.get("/me", getProfile)
+router.post("/profile", isAuthenticated, updateProfile)
+router.route("/avatar").post(isAuthenticated, updateAvatar).delete(isAuthenticated, deleteAvatar)
+
+export default router
