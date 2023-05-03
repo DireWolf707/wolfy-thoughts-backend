e3e9819648c13ec5223d8ca5e6783a3cc7bfc527
DireWolf707 <rahulsood707@gmail.com>
2023-05-04 01:54:17 +0530

    added get post route

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { isAuthenticated } from "../middlewares/auth"
-import { getPostFeed, createPost, createComment, likePost, unLikePost } from "../controllers/postController"
+import { getPost, getFeed, createPost, createComment, likePost, unLikePost } from "../controllers/postController"
 
 const router = express.Router()
-// router.use(isAuthenticated)
-router.get("/feed", getPostFeed)
+router.use(isAuthenticated)
+router.get("/feed", getFeed)
 router.post("/create", createPost)
+router.get("/:postId", getPost)
 router.route("/:postId/like").post(likePost).delete(unLikePost)
 router.post("/:postId/comment", createComment)
 
