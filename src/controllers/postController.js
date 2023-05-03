271f182701e7414d83391371f78dbfb1f82980fe
DireWolf707 <rahulsood707@gmail.com>
2023-05-04 01:56:45 +0530

    updated

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -3,29 +3,49 @@ import { prisma, redis } from "../configs"
 import { PostInput, CommentInput } from "../validators"
 import { io } from "../socket"
 
-const user = await prisma.user.findFirst()
-
-export const getPostFeed = catchAsync(async (req, res) => {
-  const { cursor } = req.body
-
+export const getFeed = catchAsync(async (req, res) => {
+  const { cursor } = req.query
   const query = {
     take: 10,
     include: {
       user: { select: { username: true, avatar: true } },
-      _count: { select: { likes: { where: { userId: user.id } } } },
+      _count: { select: { likes: { where: { userId: req.user.id } } } },
     },
     orderBy: { createdAt: "desc" },
   }
-  if (cursor) Object.assign(query, { skip: 1, cursor: { id: cursor } })
-  const posts = await prisma.post.findMany(query)
+  if (cursor) {
+    console.log("ehheh", cursor)
+    Object.assign(query, { skip: 1, cursor: { id: cursor } })
+  }
 
-  const nextCursor = posts[posts.length - 1].id
+  const posts = await prisma.post.findMany(query)
+  const nextCursor = posts[posts.length - 1]?.id
+
   res.json({ data: posts, cursor: nextCursor })
+})
+
+export const getPost = catchAsync(async (req, res) => {
+  const { postId } = req.params
+
+  const post = await prisma.post.findUniqueOrThrow({
+    where: { id: postId },
+    include: {
+      user: { select: { username: true, avatar: true } },
+      comments: {
+        include: { user: { select: { username: true, avatar: true } } },
+        orderBy: { createdAt: "desc" },
+      },
+      _count: { select: { likes: { where: { userId: req.user.id } } } },
+    },
+  })
+
+  res.json({ data: post })
 })
 
 export const createPost = catchAsync(async (req, res) => {
   const data = PostInput.parse(req.body)
-  Object.assign(data, { userId: user.id })
+  Object.assign(data, { userId: req.user.id })
+
   const post = await prisma.post.create({ data })
 
   const postKey = `post:${post.id}`
@@ -36,42 +56,41 @@ export const createPost = catchAsync(async (req, res) => {
 
 export const likePost = catchAsync(async (req, res) => {
   const { postId } = req.params
+  const data = { userId: req.user.id, postId: postId }
 
-  const data = { userId: user.id, postId: postId }
-  const like = await prisma.like.create({ data })
+  await prisma.like.create({ data })
   // const like = await prisma.like.upsert({ where: { userId_postId: data }, create: data, update: {} })
 
   const postKey = `post:${postId}`
   const likes = await redis.hIncrBy(postKey, "likes", 1)
-  io.in(postKey).emit("likes", likes)
+  io.in(postKey).emit("likes", { likes, postId })
 
-  res.json({ data: like })
+  res.end()
 })
 
 export const unLikePost = catchAsync(async (req, res) => {
   const { postId } = req.params
+  const data = { userId: req.user.id, postId: postId }
 
-  const data = { userId: user.id, postId: postId }
-  const like = await prisma.like.delete({ where: { userId_postId: data } })
+  await prisma.like.delete({ where: { userId_postId: data } })
 
   const postKey = `post:${postId}`
   const likes = await redis.hIncrBy(postKey, "likes", -1)
-  io.in(postKey).emit("likes", likes)
+  io.in(postKey).emit("likes", { likes, postId })
 
   res.end()
 })
 
 export const createComment = catchAsync(async (req, res) => {
   const { postId } = req.params
-  const data = CommentInput.parse({
-    content: "heheheh comment",
-  })
-  Object.assign(data, { userId: user.id, postId: postId })
+  const data = CommentInput.parse(req.body)
+  Object.assign(data, { userId: req.user.id, postId: postId })
+
   const comment = await prisma.comment.create({ data })
 
   const postKey = `post:${postId}`
   const comments = await redis.hIncrBy(postKey, "comments", 1)
-  io.in(postKey).emit("comments", comments)
+  io.in(postKey).emit("comments", { comments, postId })
 
-  res.end()
+  res.json({ data: comment })
 })
